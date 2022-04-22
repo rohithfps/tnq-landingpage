ba56df5ae88a318d8200e152cf79b2b9db21fa63
rohithfps <rohit.k@fingerprintscreative.com>
2022-04-22 19:13:59 +0530

    22.04.2022

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,28 @@
+var deadline = new Date("apr 29, 2022 06:30:00").getTime();
+    
+var x = setInterval(function() {
+  
+var now = new Date().getTime();
+var t = deadline - now;
+var days = Math.floor(t / (1000 * 60 * 60 * 24));
+var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((t % (1000 * 60)) / 1000);
+document.getElementById("day").innerHTML =days ;
+document.getElementById("hour").innerHTML =hours;
+document.getElementById("minute").innerHTML = minutes; 
+document.getElementById("second").innerHTML =seconds; 
+if (t < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "TIME UP";
+        document.getElementById("day").innerHTML ='0';
+        document.getElementById("hour").innerHTML ='0';
+        document.getElementById("minute").innerHTML ='0' ; 
+        document.getElementById("second").innerHTML = '0'; }
+}, 1000);
+
+
+
 // (function () {
 //     const second = 1000,
 //           minute = second * 60,
@@ -42,26 +67,3 @@
 //         }, 0)
 //     }());
 
-
-  var deadline = new Date("apr 29, 2022 06:30:00").getTime();
-    
-  var x = setInterval(function() {
-    
-  var now = new Date().getTime();
-  var t = deadline - now;
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
-  var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((t % (1000 * 60)) / 1000);
-  document.getElementById("day").innerHTML =days ;
-  document.getElementById("hour").innerHTML =hours;
-  document.getElementById("minute").innerHTML = minutes; 
-  document.getElementById("second").innerHTML =seconds; 
-  if (t < 0) {
-          clearInterval(x);
-          document.getElementById("demo").innerHTML = "TIME UP";
-          document.getElementById("day").innerHTML ='0';
-          document.getElementById("hour").innerHTML ='0';
-          document.getElementById("minute").innerHTML ='0' ; 
-          document.getElementById("second").innerHTML = '0'; }
-  }, 1000);
