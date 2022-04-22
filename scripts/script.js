e203e969f1fe42184fe3a41f9a1630c0d6e6b124
rohithfps <rohit.k@fingerprintscreative.com>
2022-04-22 17:18:55 +0530

    desktop3

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy,
-        dayMonth = "04/29/",
+        dayMonth = "04/30/",
         birthday = dayMonth + yyyy;
     
     today = mm + "/" + dd + "/" + yyyy;
@@ -27,7 +27,7 @@
                 distance = countDown - now;
   
           document.getElementById("days").innerText = Math.floor(distance / (day)),
-            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour) + 18),
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
             document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
             document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
   
